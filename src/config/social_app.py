768e17f3edb952fcc72c8da781029273d1c6cfe2
Ernur Eskermes <eskermesernur86@gmail.com>
2021-09-21 17:11:07 +0600

    deleted social_app.py-exp.py

--- src/config/social_app.py
+++ src/config/social_app.py
@@ -1,11 +1,13 @@
 from authlib.integrations.starlette_client import OAuth
 
+from . import settings
+
 oauth = OAuth()
 
 oauth.register(
     name="github",
-    client_id="ID",
-    client_secret="SECRET",
+    client_id=settings.GITHUB_CLIENT_ID,
+    client_secret=settings.GITHUB_CLIENT_SECRET,
     access_token_url="https://github.com/login/oauth/access_token",
     access_token_params=None,
     authorize_url="https://github.com/login/oauth/authorize",
